64f1ab369f36c62bd508d3104975959d9ba83f62
Brian Wong <bwong92@gmail.com>
2019-11-08 09:57:30 -0500

    brian wong

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     has_many :notes
     has_many :projects
-    has_secure_password
+    # has_secure_password
 
-    validates_presence_of :username
-    validates_uniqueness_of :username, :case_sensitive => false
+    # validates_presence_of :username
+    # validates_uniqueness_of :username, :case_sensitive => false
 
 end
